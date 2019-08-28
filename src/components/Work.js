32fbc45ca7ba4ddfa7b4227d145f2519637ce618
74thstreet <aaron.earl.harris@me.com>
2019-08-27 20:48:19 -0700

    initalized repo

---
+++ src/components/Work.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Work = () => (
+  <div className="row work">
+    <div className="three columns header-col">
+      <h1>
+        <span>Work</span>
+      </h1>
+    </div>
+
+    <div className="nine columns main-col">
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>Coffee Bean Tea & Leaf</h3>
+          <p className="info">
+            District Manager
+            <span>&bull;</span>
+            <em className="date">March 2013 - Present</em>
+          </p>
+
+          <p>
+           •	Executing standards in recruiting, hiring, training, guest services, visual merchandising, and store operations
+•	Execute plans to drive key performance indicators to maximize profitability
+•	Enhance brand loyalty by empowering team to create a natural and personable experience for customers
+•	Act as a partner between customers, sales associates, store leadership and corporate business partners
+•	Maintains a high level of customer focus and leads by example with clear and engaging communication
+•	Ensures visual directives and standards are maintained
+•	Passion for teaching associate’s product knowledge and how to apply their learnings to the customer experience
+•	Ensuring execution of operational standards within the district
+•	Actively manages succession planning with consistent attention to identifying, training and developing management candidates.
+•	 Assists in the identification, nomination and maintenance of District Training Stores as needed
+
+          </p>
+        </div>
+      </div>
+
+      <div className="row item">
+        <div className="twelve columns">
+          <h3>Burger King</h3>
+          <p className="info">
+            General Manger
+            <span>&bull;</span>
+            <em className="date">June 1999 - October 2008</em>
+          </p>
+
+          <p>
+            •	Ensures that an excellent level of Customer Service is provided in all assigned locations including the District Office where applicable. Responsible for teaching all Associates to provide a high level of Customer Service at all times.
+•	Responsible for conducting multi store visits. This includes the administration, tracking and follow-up.
+•	Ensured all company standards are executed with excellence in all assigned locations.
+•	Ensured a safe, accident-free environment for all Customers and Associates. Maintains compliance to all state, local and federal regulations.
+•	Assisted with all store related employee relations issues partnering with HR.
+•	Maintains adherence to company safety policies and ensures the safety of associates and customer.
+•	Assesses and escalates any inventory level discrepancies.
+•	Coordinated staff to make store hours and customer flow run in a professional manner
+•	Conducted new hire interviews and training according to districts needs
+•	Assisted GM’s with quarterly forecast, weekly schedules and with monitoring labor to be consistent with store sales and productivity guidelines.
+•	Maintain positive working relationships with direct reports, peers, supervisors, suppliers, and customers effectively handling complex or difficult situations involving others.
+•	Responsible for second interviewing process for management level new hire.
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Work;
